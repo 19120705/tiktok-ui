e8fc21339ff6a98a4bf1f01e97f0f38f9c2c5a6b
19120705 <tranviettuan2k1@gmail.com>
2023-04-09 12:01:54 +0700

    create header ui

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,24 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import linkImages from '~/assets/images/955024.jpg';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src={linkImages} alt="Hoaa" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import Button from '~/components/Button';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <Button outline large>
+                Log in
+            </Button>
+        </aside>
+    );
 }
 
 export default Sidebar;
